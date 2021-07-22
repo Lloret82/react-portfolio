b675b85fae75ee4056e05b15a56490e5c4dcaa78
Lloret82 <Andrea.fullstack@gmail.com>
2021-07-22 15:01:42 +0100

    Add files via upload

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,22 +1,67 @@
+import emailjs from "emailjs-com";
 import "./style.css";
+import React from "react";
+
+
+
+
 
 const About = () => {
+  function sendEmail(e) { 
+    e.preventDefault()
+    ;
+
+    emailjs
+      .sendForm(
+        "service_rw70k2d",
+        "template_apy6h9r",
+        e.target,
+        "user_etEj26siyXh7iVOifHYMb"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
+  
+  
+  
+
+
   return (
     <div className="contact">
-      <h1>Contact me!</h1>
-      <img className="img"
-        src="https://images.unsplash.com/photo-1528747045269-390fe33c19f2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2FsbCUyMG1lfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
-        
-      ></img>
+      <div
+        className="container"
+        style={{
+          marginTop: "50px",
+          width: "50%",
+        }}
+      >
+        <h1 style={{ marginTop: "25px" }}>Contact Form</h1>
+        <form className="form-style-6" onSubmit={sendEmail} >
+          <label>name</label>
+          <input  type="text" name="name" className="form-control" />
 
-      
+          <label>Email</label>
+          <input  type="email" name="user_email" className="form-control"  />
 
-      <h4>Email</h4>
-      <p className="cont">andrea.fullstack@gmail.com</p>
-
+          <label>Message</label>
+          <textarea  name="message" rows="4" className="form-control" />
+          <input
+            type="submit"
+            
+            className="form-control btn btn-primary"
+            style={{ marginTop: "30px" }}
+            >
+              
+          </input>
+        </form>
+      </div>
       
     </div>
   );
 };
+
+
 
 export default About;
